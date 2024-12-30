beb21f7c63b1fe87240fc08fd30a9b0eef696790
abu <abusayed8531@gmail.com>
2024-12-30 14:36:40 +0600

    set template

--- oop.php
+++ oop.php
@@ -1,2 +1,34 @@
 <?php
-class Book {}
+class Book
+{
+    private $title, $availableCopies;
+    public function __construct($title, $availableCopies)
+    {
+        $this->title = $title;
+        $this->availableCopies = $availableCopies;
+    }
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    public function getAvailableCopies()
+    {
+        return $this->availableCopies;
+    }
+    public function borrowBook() {}
+    public function returnBook() {}
+}
+class Member
+{
+    private $name;
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function borrowBook() {}
+    public function returnBook() {}
+}
